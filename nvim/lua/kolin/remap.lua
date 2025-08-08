90e8e498e976a1c045736a2473984015504df12f
Kolin63 <theverylastgoodname@gmail.com>
2025-08-07 21:37:01 -0400

    Remove nop of Q

--- nvim/lua/kolin/remap.lua
+++ nvim/lua/kolin/remap.lua
@@ -29,9 +29,6 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 vim.keymap.set({ "n", "v" }, "<leader>D", "\"_dd")
 
--- Get Rid of Q
-vim.keymap.set("n", "Q", "<nop>")
-
 -- Leader S over a word to replace it
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
