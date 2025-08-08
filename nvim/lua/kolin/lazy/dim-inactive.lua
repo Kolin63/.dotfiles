e7173467d4ddcfad3ddea45e2e6a4a456fafb088
Kolin63 <theverylastgoodname@gmail.com>
2025-08-07 21:58:39 -0400

    Nvim Changes

--- nvim/lua/kolin/lazy/dim-inactive.lua
+++ nvim/lua/kolin/lazy/dim-inactive.lua
@@ -1,6 +1,8 @@
 return {
     'blueyed/vim-diminactive',
 
+    enabled = false,
+
     config = function ()
         -- Gruvbox Colors!!!!!
         vim.cmd("hi ColorColumn ctermbg=0 guibg=#1d2021")
