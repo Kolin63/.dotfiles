b76aa77056fcf56da08a93e437e835cdfb5b54c1
Kolin63 <theverylastgoodname@gmail.com>
2025-08-16 16:47:25 -0400

    Add Neovim Config to Load Plugins in Development

--- nvim/lua/kolin/init.lua
+++ nvim/lua/kolin/init.lua
@@ -5,3 +5,9 @@ vim.g.maplocalleader = "\\"
 require("kolin.init_lazy")
 require("kolin.remap")
 require("kolin.set")
+
+vim.opt.rtp:append("~/Projects/invisible-ink")
+require("invisible-ink")
+
+vim.opt.rtp:append("~/Projects/burrito")
+require("burrito")  -- or whatever the plugin's entry point is
