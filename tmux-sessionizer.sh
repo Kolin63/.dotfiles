bc27b2747ceaf37e32e29fd0078c3a54cdb20dff
Kolin63 <theverylastgoodname@gmail.com>
2025-09-07 22:06:37 -0400

    Add School Folder to Tmux Sessionizer

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/Projects ~/Tests ~/Notes ~/.dotfiles -mindepth 0 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/Projects ~/Tests ~/Notes ~/School ~/.dotfiles -mindepth 0 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
