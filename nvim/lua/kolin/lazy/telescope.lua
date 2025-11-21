e78f88096ff97af7e12bf4528414607213058dbe
Kolin63 <theverylastgoodname@gmail.com>
2025-11-21 00:54:00 -0500

    Change Telescope Config

--- nvim/lua/kolin/lazy/telescope.lua
+++ nvim/lua/kolin/lazy/telescope.lua
@@ -1,16 +1,17 @@
 return {
-    "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
 
-    config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") }) end,
-            { desc = 'Telescope find string occurence' })
-        end
-    }
+  config = function()
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>pf', function()
+      builtin.find_files({ no_ignore = true }) end,
+      { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") }) end,
+      { desc = 'Telescope find string occurence' })
+  end
+}
