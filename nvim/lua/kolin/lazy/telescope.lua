217628a3036c54501daec5fc8b4d480cce5a0d51
Kolin63 <theverylastgoodname@gmail.com>
2026-05-23 16:48:40 -0400

    Disable file previewer in file finder

--- nvim/lua/kolin/lazy/telescope.lua
+++ nvim/lua/kolin/lazy/telescope.lua
@@ -6,12 +6,22 @@ return {
   },
 
   config = function()
+    require('telescope').setup({
+      pickers = {
+        find_files = {
+          previewer = false
+        }
+      }
+    })
+
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>pf', function()
-      builtin.find_files() end,
-      { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>ps', function()
-      builtin.live_grep() end,
-      { desc = 'Telescope find string occurence' })
+
+    vim.keymap.set('n', '<leader>pf',
+    function() builtin.find_files({ hidden = true }) end,
+    { desc = 'Telescope find files' })
+
+    vim.keymap.set('n', '<leader>ps',
+    function() builtin.live_grep() end,
+    { desc = 'Telescope find string occurence' })
   end
 }
